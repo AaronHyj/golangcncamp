3d950f49fff3c9e1f22b4d5c3109cb79c9842f51
Aaronhyj <Aaronhyjk@163.com>
2024-06-20 08:29:26 +0800

    homework 1

--- examples/module1/slice/forrange/main.go
+++ examples/module1/slice/forrange/main.go
@@ -5,13 +5,21 @@ import (
 )
 
 func main() {
-	mySlice := []int{10, 20, 30, 40, 50}
-	for _, value := range mySlice {
-		value *= 2
+	// mySlice := []int{10, 20, 30, 40, 50}
+	// for _, value := range mySlice {
+	// 	value *= 2
+	// }
+	// fmt.Printf("mySlice %+v\n", mySlice)
+	// for index, _ := range mySlice {
+	// 	mySlice[index] *= 2
+	// }
+	// fmt.Printf("mySlice %+v\n", mySlice)
+	str := [5]string{"i", "am", "stupid", "and", "weak"}
+	for index, value := range str {
+		fmt.Println(index, value)
 	}
-	fmt.Printf("mySlice %+v\n", mySlice)
-	for index, _ := range mySlice {
-		mySlice[index] *= 2
-	}
-	fmt.Printf("mySlice %+v\n", mySlice)
+	// 修改数据内容为：{"i", "am", "smart", "and", "strong"}
+	str[2] = "smart"
+	str[4] = "strong"
+	fmt.Printf("str %+v\n", str)
 }
